01372e1a59fbd99200d751a78d115499b6d39733
cabtest <sobolat@yahoo.com>
2018-02-25 05:12:32 +0000

    connect commit

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $servername = "tcp:rafflex.database.windows.net,1433";
 $username = "rafflex";
 $password = "RaffleTest2017";
-$databasename = "raffletestdb";
+$databasename = "rafflex";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $databasename);
 
